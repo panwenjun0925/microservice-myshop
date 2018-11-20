5daed644795c6ae0ca07d6b4acb1965fe9c6b695
0725youqi <1399211854@qq.com>
2018-11-20 16:39:00 +0800

    更改了包结构

---
+++ myshop-product-server/src/main/java/tk/mybatis/mapper/MyMapper.java
@@ -0,0 +1,24 @@
+package tk.mybatis.mapper;
+
+import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.MySqlMapper;
+
+/**
+ * @Auther: YouQi
+ * @Date: 2018/11/19 17:14
+ * @Description:
+ * @Version: 1.0
+ */
+
+/**
+ * 自己的 Mapper
+ * 特别注意，该接口不能被扫描到，否则会出错
+ * <p>Title: MyMapper</p>
+ * <p>Description: </p>
+ *
+ * @author Lusifer
+ * @version 1.0.0
+ * @date 2018/5/29 0:57
+ */
+public interface MyMapper<T> extends Mapper<T>, MySqlMapper<T> {
+}
